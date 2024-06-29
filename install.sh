f8c795a7764a1eb8d9aeb0864696de8c5cbbb733
mc-miner <18306756607@163.com>
2024-06-29 12:36:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="3.9.94"
-DOWNLOAD_HOST="https://github.com/mc-miner/MCMiner/blob/main/linux"
+DOWNLOAD_HOST="https://github.com/mc-miner/MCMiner//raw/main/linux"
 ORIGIN_EXEC="MCMiner-${VERSION}"
 
 SERVICE_NAME="MCMinerservice"
